e749c9a6453c896ce43ecafdc7ae812bee263b48
Adam <adamdials@unity3d.com>
2015-10-06 12:26:42 -0500

    1

---
+++ Assets/Editor/Colors.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public class Colors : MonoBehaviour {
+
+    public enum Color
+    {
+        red,
+        white,
+        blue
+    };
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
